3d7427f132cc4d3c1942c388e534117307af9d3a
Natali Schwartz <schwartznatali@gmail.com>
2025-02-22 15:19:20 -0300

    create the Navbar and Inicio Component

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './Components/Navbar.jsx';
+import Inicio from './Components/Publico/Inicio.jsx';
 
 function App() {
   
   return (
     <div id="app">
       <Navbar/>
+      <Inicio/>
     </div>
    
   )
---
+++ Front/src/Components/Publico/Inicio.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import "./Inicio.css";
+
+const Inicio = () => {
+  return (
+    <>
+      <div className="div_img_inicio">
+        <div className="img_text_inicio">
+          <h1 className="titulo_inicio">Tu mascota, nuestra prioridad</h1>
+
+          <div className="parrafo_container">
+            <p className="parrafo_inicio">
+              Clínica veterinaria integral, líderes en atención especializada
+              para tu mascota. Agenda tu consulta hoy mismo.
+            </p>
+
+            <div>
+              <Link>
+                <button id="btn-consulta-inicio">Agendar Turno</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="container">
+        <h2 id="titulo_secundario_inicio">Nuestros Servicios</h2>
+        <div className="services_container">
+                    <div className="services_content">
+                        <i className="fa-solid fa-user-doctor services_img"></i>
+                        <h3 className="services_title ">Especialistas</h3>
+                        <p className="services_description">Ofrecemos diagnósticos precisos y tratamientos avanzados para garantizar la salud y el bienestar de tu mascota en cada etapa de su vida.</p>
+                    </div>
+
+                    <div className="services_content">
+                        <i className="fa-solid fa-house-medical-circle-check services_img"></i>
+                        <h3 className="services_title ">Internación</h3>
+                        <p className="services_description">
+                        Contamos con instalaciones equipadas para proporcionar confort y seguridad, asegurando que cada paciente reciba los cuidados necesarios durante su recuperación.
+                        </p>
+                    </div>
+
+                    <div className="services_content">
+                        <i className="fa-solid fa-kit-medical services_img"></i>
+                        <h3 className="services_title ">Cirugía</h3>
+                        <p className="services_description">Brindamos anestesia segura, monitoreo constante y un plan de recuperación personalizado para que tu mascota vuelva a casa en óptimas condiciones. Su salud es nuestra prioridad. </p>
+                    </div>
+
+                   
+                   
+                </div>
+      </section>
+
+      
+      
+    </>
+  );
+};
+
+export default Inicio;
